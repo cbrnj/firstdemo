00c2e5bc7b3df0b9208e8f2e4c0fbbcc120d567c
cbr.nj170922@gmail.com <Cbr170922@>
2024-10-25 09:35:37 +0800

    测试3

--- src/demo.java
+++ src/demo.java
@@ -2,6 +2,6 @@ import java.awt.*;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("Hello World 123");
+        System.out.println("Hello World 123456");
     }
 }
